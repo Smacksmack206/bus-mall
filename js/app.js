19abd154b7a5d17f5d6d6a13700da29dc2a4792f
Cedric Vallieu <cvallieu94@gmail.com>
2021-11-01 10:15:00 -0700

    Fixed to complete all requirements for lab11

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ let image2 = document.querySelector('section img:nth-child(2)');
 let image3 = document.querySelector('section img:nth-child(3)');
 
 
+
 let allProducts = [];
 const clicksAllowed = 3;
 let clicks = 0;
@@ -21,7 +22,7 @@ function Product(name, fileExtention = 'jpg', views) {
   this.name = name;
   this.src = `img/${name}.${fileExtention}`;
   this.views = 0;
-//2.a In the constructor function define a property to hold the number of times a product has been clicked.//
+  //2.a In the constructor function define a property to hold the number of times a product has been clicked.//
   this.likes = 0;
   this.clicks = 0;
   allProducts.push(this);
@@ -51,109 +52,103 @@ new Product('water-can');
 new Product('wine-glass');
 
 
-image1.src = allProducts[0].src;
-image2.src = allProducts[1].src;
+// image1.src = cover.png;
+// image2.src = cover.png;
+// image3.src = "img/cover.png";
 
 function selectRandomProduct() {
-return Math.floor(Math.random() * allProducts.length);
+  return Math.floor(Math.random() * allProducts.length);
 }
 
 //renders products to myContainer//
+
 function renderProduct() {
-let Product1 = selectRandomProduct();
-let Product2 = selectRandomProduct();
-let Product3 = selectRandomProduct();
-//For each of the three images, increment its property of times it has been shown by one.//
-allProducts[Product1].views++;
-allProducts[Product2].views++;
-allProducts[Product3].views++;
-console.log(allProducts[Product1].views);
-console.log(allProducts[Product2].views);
-console.log(allProducts[Product3].views);
+  let Product1 = selectRandomProduct();
+  let Product2 = selectRandomProduct();
+  let Product3 = selectRandomProduct();
 
-//array.method. include() look this up will be useful//
+  //For each of the three images, increment its property of times it has been shown by one.//
+  allProducts[Product1].views++;
+  allProducts[Product2].views++;
+  allProducts[Product3].views++;
+  // console.log(allProducts[Product1].views);
+  // console.log(allProducts[Product2].views);
+  // console.log(allProducts[Product3].views);
 
-//makes sure products are not the same//
-while (Product1 === Product2 || Product2 === Product3 || Product1 === Product3) {
-    
+  //array.method. include() look this up will be useful//
+
+  //makes sure products are not the same//
+  while (Product1 === Product2 || Product2 === Product3 || Product1 === Product3) {
+
     Product2 = selectRandomProduct();
     Product3 = selectRandomProduct();
-    // Product1.views++;
 
 
-}
 
-image1.src = allProducts[Product1].src;
-image1.alt = allProducts[Product1].name;
-image1.views = allProducts[Product1].views;
-image2.src = allProducts[Product2].src;
-image2.alt = allProducts[Product2].name;
-image2.views = allProducts[Product2].views;
-image3.src = allProducts[Product3].src;
-image3.alt = allProducts[Product3].name;
-image3.views = allProducts[Product3].views;
+  }
+
+  image1.src = allProducts[Product1].src;
+  image1.alt = allProducts[Product1].name;
+  image1.views = allProducts[Product1].views;
+  image2.src = allProducts[Product2].src;
+  image2.alt = allProducts[Product2].name;
+  image2.views = allProducts[Product2].views;
+  image3.src = allProducts[Product3].src;
+  image3.alt = allProducts[Product3].name;
+  image3.views = allProducts[Product3].views;
 }
 
 function handleProductClick(event) {
   if (event.target === myContainer) {
     alert('Please click on an image');
   }
-  clicks++
-  console.log(clicks);
+  
+  // console.log(clicks);
   let clickedProduct = event.target.alt;
-  console.log(event.target.alt);
+  // console.log(event.target.alt);
   for (let i = 0; i < allProducts.length; i++) {
-  if (clickedProduct === allProducts[i].name) {
-    //2.b After every selection by the viewer, update the newly added property to reflect if it was clicked.//
-    allProducts[i].likes++;
-    rounds++;
-    break
+    if (clickedProduct === allProducts[i].name) {
+      //2.b After every selection by the viewer, update the newly added property to reflect if it was clicked.//
+      allProducts[i].likes++;
+      rounds++;
+      allProducts[i].clicks++
+      // console.log(rounds);
+      break
+    }
+  }
+  if (rounds > limit) {
+    alert('You passed round 25 - it is over');
+    //   image1.src = '';
+    // image2.src = '';
+    // image3.src = '';
+    // image1.alt = '';
+    // image2.alt = '';
+    // image3.alt = '';
+    myContainer.removeEventListener('click', handleProductClick);
+    return;
+  } else if (rounds < limit) {
+    renderProduct();
   }
 }
-renderProduct();
-}
-renderProduct();
+// renderProduct();
 
-function handleButtonClick() {
-  for (let i = 0; i < allProducts.length; i++) {
+function handleButtonClick(e) {
+
+    for (let i = 0; i < allProducts.length; i++) {
     let li = document.createElement('li');
-    li.textContent = `${allProducts} `
+    li.textContent = `${allProducts[i].name} had ${allProducts[i].clicks} votes, and was seen ${allProducts[i].views} times.`;
+    results.appendChild(li);
   }
+  
+  
 }
 
-if (clicks === clicksAllowed) {
-myContainer.removeEventListener('click', handleProductClick);
 
-}
 myButton.addEventListener('click', handleButtonClick);
 
 myContainer.addEventListener('click', handleProductClick);
 
+// my view results is not working need to get help with function handleButtonClick(event)
 
+// Listing results with that event handler is the last req I need to finish
 
-// create a loop towards 25 rounds
-// for (let i = 0; i < 1; i++) {
-//   renderProduct();
-// for (let rounds = 0; rounds < limit; rounds++) {
-//   function handleProductClick(event) {
-//     if (event.target === myContainer) {
-//       alert('Please click on an image');
-//     }
-//     clicks++
-//     console.log(clicks);
-//     let clickedProduct = event.target.alt;
-//     console.log(event.target.alt);
-//     for (let i = 0; i < allProducts.length; i++) {
-//     if (clickedProduct === allProducts[i].name) {
-//       //2.b After every selection by the viewer, update the newly added property to reflect if it was clicked.//
-//       allProducts[i].likes++;
-//       rounds++;
-//       break
-//     }
-//   }
-//   renderProduct();
-//   }
-  
-// }
-
-// }
